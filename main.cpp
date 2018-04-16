12ab9d5011aeb1432e999ff35ddca8f4f4b107e2
Juan Perez <juanl1996@hotmail.com>
2018-04-15 21:28:01 -0400

    Proyecto full

--- main.cpp
+++ main.cpp
@@ -1,50 +1,51 @@
 #include <iostream>
 #include "Lib/Cola.h"
+#include "Lib/Genericas/VGeneral.h"
 #include <string>
+using namespace std;
 int main() {
-    Cola<string> cola;
-    Nodo<string> *aux;
-    //cola.Remover(aux);
-    //cout <<"Prioridad: "<< aux->ObtPrioridad()<<endl;
-    //cout <<"Info "<< aux->ObtInfo()<<endl;
-    cola.InsertarConPrioridad("25",1);
-    cola.InsertarConPrioridad("26",2);
-    cola.InsertarConPrioridad("21",1);
-    cola.InsertarConPrioridad("32",3);
-    cola.InsertarConPrioridad("19",4);
-    cola.InsertarConPrioridad("23",1);
-    cola.InsertarConPrioridad("17",3);
-    cola.InsertarConPrioridad("26",2);
-
-    for(int i=0;i<8;i++){
-        aux = new Nodo<string>;
-        cola.Remover(aux);
-        cout<<"Prioridad cliente "<<i+1<<": "<<aux->ObtPrioridad()<<"\n valor: "<<aux->ObtInfo()<<endl;
-
-    }
-
-
-
-
-    /*
-    Cola<int> cola;
+    Cola<int > cola;
     Nodo<int> *aux;
-    aux = new Nodo<int>;
-    cola.InsertarNodo(cola.CrearNodo(2,5));
-    cola.InsertarNodo(cola.CrearNodo(6,7));
-    cola.InsertarNodo(cola.CrearNodo(1,2));
+    VGeneral vg;
+    int opc;
+    vg.Pausa();
+    do
+    {
+        vg.Limpiar();
+        vg.ImprimirLineasBlanco(1);
+        vg.Limpiar();
+        vg.ImprimirEncabezado("\n      M E N U  O P C I O N E S\n","      =======  ===============");
+        vg.ImprimirMensaje("   1. CARGAR COLA\n");
+        vg.ImprimirMensaje("   2. IMPRIMIR COLA B\n");
+        vg.ImprimirMensaje("   3 FINALIZAR\n\n");
+        opc = vg.LeerValidarNro("   SELECCIONE SU OPCION : ",1,3);
+        switch (opc)
+        {
+            case 1: {
+                cola.InsertarConPrioridad(25,1);
+                cola.InsertarConPrioridad(26,2);
+                cola.InsertarConPrioridad(21,1);
+                cola.InsertarConPrioridad(32,3);
+                cola.InsertarConPrioridad(19,4);
+                cola.InsertarConPrioridad(23,1);
+                cola.InsertarConPrioridad(17,3);
+                cola.InsertarConPrioridad(26,2);
+                vg.ImprimirMensaje("   Cola Cargada Con EXITO!!");
+            }
+                break;
+            case 2: {
+                for(int i=0;i<8;i++){
+                    aux = new Nodo<int>;
+                    cola.Remover(aux);
+                    cout<<"Prioridad cliente "<<i+1<<": "<<aux->ObtPrioridad()<<" Valor: "<<aux->ObtInfo()<<endl;
 
-    cola.Remover(aux);
-    cout << "Primer Valor: " << aux->ObtInfo() <<endl;
-    cout << "Prioridad: " << aux->ObtPrioridad()<< endl;
+                }
 
-    cola.Remover(aux);
-    cout << "Segundo Valor: " << aux->ObtInfo() <<endl;
-    cout << "Prioridad: " << aux->ObtPrioridad()<< endl;
+            }
+                break;
+        }
+    }
+    while(opc != 3);
 
-    cola.Remover(aux);
-    cout << "Tercer Valor: " << aux->ObtInfo() <<endl;
-    cout << "Prioridad: " << aux->ObtPrioridad()<< endl;
-    */
     return 0;
 }
